c850e55069189ee41d60290dbcc130a951cbab88
Dilshan Shivantha <shiwanthadilshan3@gmail.com>
2023-09-18 23:52:38 +0530

    spring web mvc convention

---
+++ GlobalTechAPI/src/main/java/lk/ijse/gdse/globaltechapi/WebApplicationInitializer.java
@@ -0,0 +1,23 @@
+package lk.ijse.gdse.globaltechapi;
+
+import lk.ijse.gdse.globaltechapi.config.WebAppConfig;
+import lk.ijse.gdse.globaltechapi.config.WebAppRootConfig;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebApplicationInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{WebAppRootConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebAppConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+}
