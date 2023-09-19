3290d1c78eff5e2a910d5466f10a827837dbe3da
Dilshan Shivantha <shiwanthadilshan3@gmail.com>
2023-09-19 13:09:23 +0530

    Entity class created and successfully table is created in the database

---
+++ GlobalTechAPI/src/main/java/lk/ijse/gdse/globaltechapi/entity/TechLead.java
@@ -0,0 +1,31 @@
+package lk.ijse.gdse.globaltechapi.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.lang.Nullable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "techlead")
+public class TechLead implements SuperEntity {
+
+    @Id
+    private String employeeId;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private String contactNo;
+    @Column(nullable = false)
+    private String email;
+    @Column(nullable = false)
+    private String profileImage;
+
+}
