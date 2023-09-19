db157edbb66f6e99c448ee526613a3626a719841
Dilshan Shivantha <shiwanthadilshan3@gmail.com>
2023-09-19 13:07:32 +0530

    repository package update

---
+++ GlobalTechAPI/src/main/java/lk/ijse/gdse/globaltechapi/repository/TechLeadRepository.java
@@ -0,0 +1,9 @@
+package lk.ijse.gdse.globaltechapi.repository;
+
+import lk.ijse.gdse.globaltechapi.entity.TechLead;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TechLeadRepository extends JpaRepository<TechLead,String> {
+}
