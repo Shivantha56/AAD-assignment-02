600bdb5b4de602e589acfc16dc7b8af82e5f634c
Dilshan Shivantha <shiwanthadilshan3@gmail.com>
2023-09-21 13:46:07 +0530

    exceptoion class created

---
+++ GlobalTechAPI/src/main/java/lk/ijse/gdse/globaltechapi/exception/InvalidException.java
@@ -0,0 +1,7 @@
+package lk.ijse.gdse.globaltechapi.exception;
+
+public class InvalidException extends RuntimeException{
+    public InvalidException(String message) {
+        super(message);
+    }
+}
